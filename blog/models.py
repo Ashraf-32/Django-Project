e1ac2db103013bbe844a136cc753bbd58bdb1856
Ashraf-32 <syedishmam12@gmail.com>
2020-07-13 14:53:39 +0600

    added the change

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    photo = models.ImageField(default='default.jpg', upload_to='uploaded_pics')
+    photo = models.ImageField(upload_to='uploaded_pics', blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
